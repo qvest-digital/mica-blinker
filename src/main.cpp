cb40bf99c483ec378a342e2c665e8de8ef7e3217
Hannes Rohde <hannes.rohde@web.de>
2018-09-21 21:41:35 +0200

    brought back old frame timing

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include "constants.h"
 #include "Player.h"
 
-Player player = Player(0, 1, 100);;
+Player player = Player(0, 0.25f, 100);
 
 CRGB frameBuffer[NUM_PIXELS];
 
@@ -32,56 +32,32 @@ void handleInputs() {
   }
 }
 
+static const unsigned long FRAME_MICROS = 1000000 / MAX_FPS;
+
 void loop() {
+  unsigned long startMicros = micros();
+
+  /////////////////////////////////////////////////////////////////////////////
 
   handleInputs();
 
-  player.tick();
-
+  // reset all leds
   fill_solid(frameBuffer, NUM_PIXELS, CRGB::Black);
+
+  player.tick();
   player.render(frameBuffer);
 
   FastLED.show();
 
-  delay(10);
+  //////////////////////////////////////////////////////////////////////////////
+
+  unsigned long stopMicros = micros();
+
+  // sleep for remaining time of this frame to cap FPS
+  if (stopMicros >= startMicros) {
+    unsigned long durationMicros = stopMicros - startMicros;
+    if (durationMicros < FRAME_MICROS) {
+      delayMicroseconds(FRAME_MICROS - durationMicros);
+    }
+  }
 }
-
-
-// #include <FastLED.h>
-
-// #define MAX_FPS  200
-// #define NUM_LEDS 144
-
-// static unsigned long FRAME_MICROS = 1000000 / MAX_FPS;
-
-// static CRGB leds[NUM_LEDS];
-
-// void setup() {
-//   CLEDController &controller = FastLED.addLeds<SK9822, BGR>(leds, NUM_LEDS);
-//   controller.clearLeds(NUM_LEDS);
-// }
-
-// void loop() {
-//   unsigned long startMicros = micros();
-
-//   /////////////////////////////////////////////////////////////////////////////
-
-//   // reset all leds
-//   fill_solid(leds, NUM_LEDS, CRGB::Black);
-
-//   // TODO
-
-//   FastLED.show();
-
-//   //////////////////////////////////////////////////////////////////////////////
-
-//   unsigned long stopMicros = micros();
-
-//   // sleep for remaining time of this frame to cap FPS
-//   if (stopMicros >= startMicros) {
-//     unsigned long durationMicros = stopMicros - startMicros;
-//     if (durationMicros < FRAME_MICROS) {
-//       delayMicroseconds(FRAME_MICROS - durationMicros);
-//     }
-//   }
-// }
