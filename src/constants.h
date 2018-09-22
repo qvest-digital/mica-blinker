924ea2c5bcd01ae284a6b4a7770fffca6f85c6fd
Ibragim Kuliev <i.kuliev@tarent.de>
2018-09-22 12:54:46 +0200

    reverted constant

--- src/constants.h
+++ src/constants.h
@@ -3,7 +3,7 @@
 #define _CONSTANTS_H
 
 #define MAX_FPS  200                // cap for frames-per-second
-#define NUM_PIXELS 72             // number of pixels in strip
+#define NUM_PIXELS 144             // number of pixels in strip
 
 #define MAX_NODES 50                // maximum number of Nodes in the world
 
